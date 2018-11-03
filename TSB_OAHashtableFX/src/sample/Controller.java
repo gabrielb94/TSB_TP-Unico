6e3394a754a2cfce71b7f6169d8122e665e82695
Keyssi <cahuana.key@gmail.com>
2018-11-02 21:01:23 -0300

    en pdf especificaciones se encuentra resaltado lo que esta listo
    se tiene que revisar los caso de abrir la subventana.
    No pude probarlo por el error mencionado.

--- TSB_OAHashtableFX/src/sample/Controller.java
+++ TSB_OAHashtableFX/src/sample/Controller.java
@@ -5,10 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert;
+
+import java.io.File;
 
 public class Controller implements Initializable {
 
     private Archivo archivo;
+    private TSBArrayList lista;
+    private  TSBHashtable tablaHash;
+
 
     public Button btnNuevaTabla;
     public Button btnAbrirTabla;
@@ -22,10 +28,11 @@ public class Controller implements Initializable {
     public TextField txtCant;
     public label lbl_DatosPalabra;
 
-    public void btnBuscar_click (ActionEvent evento)
+    public void btnBuscarClic (ActionEvent evento)
     {
+        //habilita
         txtFrec.setDisable(false);
-        txtCant.setDisable(false);
+        txtFrec.setEditable(true);
         if (txt_buscar.getText().isEmpty())
         {
             lbl_DatosPalabra.setText("No se ingreso ninguna palabra");
@@ -36,20 +43,84 @@ public class Controller implements Initializable {
             if (archivo.buscarPalabra(palabra))
             {
                 int valor = hash.get(palabra);
-                if(valor == 1)
-                    lbl_DatosPalabra.setText("La palabra " + palabra + " aparece " + valor + " vez.");
-                else
-                    lbl_DatosPalabra.setText("La palabra " + palabra + " aparece " + valor + " veces.");
-            }
-            else
-            {
-                lbl_DatosPalabra.setText("Palabra NO encontrada");
-            }
-        }
+               txtFrec.setText(valor)
+             }
+         }
+    }
+    public void btnCrearTablaVaciaClic (Action evento)
+    {
+        //habilita
+    txtNombre.setDisable(false);
+    txtCant.setDisable(false);
+    txtCant.setEditable(true);
+    btnAgregarArchivo.setDisable(false);
+    btnGuardar.setDisable(false);
+    btnBuscar.setDisable(false);
+
+    //crear tashtable vacia
+
+
+    }
+
+    public void btnAbrirTablaClic (ActionEven e)
+    {
+        //habilita
+        txtNombre.setDisable(false);
+        txtNombre.setEditable(true);
+        txtCant.setDisable(false);
+        txtCant.setEditable(true);
+        btnAgregarArchivo.setDisable(false);
+        btnGuardar.setDisable(false);
+        btnBuscar.setDisable(false);
+
+        //abrir subventana para buscar tabla serializada es archivo
+        //txtBuscar.setEditable(true);
+        FileChooser fc= new FileChooser();
+        FileChooser.ExtensionFilter fe= new FileChooser.ExtensionFilter("doc",".txt");
+// lo veremos en otra clase :)
+        fc.setSelectedExtensionFilter(fe);  //FileChooser es la ventanita o algo asi
+        fc.setTitle("Seleccione un archivo");
+        File file= fc.showOpenDialog(null);
+        archivo= new Archivo(file);
+        String nombre= file.getName();
+      //  lblArchivo.setText(nombre);
+        lista=archivo.leer();
+        //txtArchivo.setText(lista.toString());
+    }
+
+    public void  btnAgregarArchivoClic (ActionEven e)
+    {
+        //txtBuscar.setEditable(true);
+        FileChooser fc= new FileChooser();
+        FileChooser.ExtensionFilter fe= new FileChooser.ExtensionFilter("doc",".txt");
+// lo veremos en otra clase :)
+        fc.setSelectedExtensionFilter(fe);  //FileChooser es la ventanita o algo asi
+        fc.setTitle("Seleccione un archivo");
+        File file= fc.showOpenDialog(null);
+        archivo= new Archivo(file);
+        String nombre= file.getName();
+        //falta actualizar cantidad de palabras
+        //falta validar
+
+
+    }
+
+    public void btnGurdarCambiosClic (ActionEven e)
+    {
+//falta validar
+
+        //si todo es correcto muestra el mensaje de exito
+
+        Alert a= new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle("Guardado");
+        a.setHeaderText(null);
+        a.setContentText("El archivo ha sido agregado con éxito!!");
+        a.showAndWait();
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
 
 }
